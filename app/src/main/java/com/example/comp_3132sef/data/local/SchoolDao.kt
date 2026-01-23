655c0950cd8567f5e07afbcaea93afd24830d547
s7e7a <ajunsui.peach@gmail.com>
2026-01-24 07:13:02 +0800

    changing for store all data from api

--- app/src/main/java/com/example/comp_3132sef/data/local/SchoolDao.kt
+++ app/src/main/java/com/example/comp_3132sef/data/local/SchoolDao.kt
@@ -17,5 +17,15 @@ interface SchoolDao {
 
     @Query("DELETE FROM schools")
     suspend fun clearAll()
+
+    @Query("""
+        SELECT * FROM schools 
+        WHERE chineseName LIKE '%' || :query || '%' 
+        OR englishName LIKE '%' || :query || '%' 
+        OR chineseAddress LIKE '%' || :query || '%' 
+        OR telephone LIKE '%' || :query || '%'
+        OR chineseDistrict LIKE '%' || :query || '%'
+    """)
+    fun searchSchools(query: String): Flow<List<SchoolEntity>>
 }
 
