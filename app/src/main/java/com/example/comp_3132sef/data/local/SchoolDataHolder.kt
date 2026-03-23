11adf6150fbebd421dd24b78d703da9b0852af84
s7e7a <ajunsui.peach@gmail.com>
2026-03-23 13:12:14 +0800

    User Data Temporary SchoolDataHolder selectedSchool isZh query currencySelectedFilters

---
+++ app/src/main/java/com/example/comp_3132sef/data/local/SchoolDataHolder.kt
@@ -0,0 +1,17 @@
+package com.example.comp_3132sef.data.local
+
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.util.Locale
+
+object SchoolDataHolder {
+    var selectedSchool: SchoolEntity? = null
+    var isZh by mutableStateOf(Locale.getDefault().language == "zh")
+    var query by mutableStateOf({ "" })
+    var currencySelectedFilters by mutableStateOf<Map<String, List<String>>>(emptyMap())
+    
+
+}
