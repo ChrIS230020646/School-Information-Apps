8b777109f41289b56231731f5f3a30500bfbfec9
s7e7a <ajunsui.peach@gmail.com>
2026-03-23 13:13:17 +0800

    new Filter checked set

---
+++ app/src/main/java/com/example/comp_3132sef/ui/search/FilterState.kt
@@ -0,0 +1,31 @@
+package com.example.comp_3132sef.ui.search
+
+import com.example.comp_3132sef.data.local.Filter.FilterGroup
+
+class FilterState(
+    val session: FilterGroup,
+    val gender: FilterGroup,
+    val religion: FilterGroup,
+    val category: FilterGroup,
+    val district: FilterGroup,
+    private val onUpdate: (FilterType, List<Boolean>) -> Unit
+) {
+    enum class FilterType { SESSION, GENDER, RELIGION, CATEGORY, DISTRICT }
+
+    // Setter: 更新特定類別的勾選狀態
+    fun updateChecked(type: FilterType, index: Int, isChecked: Boolean) {
+        val currentGroup = when (type) {
+            FilterType.SESSION -> session
+            FilterType.GENDER -> gender
+            FilterType.RELIGION -> religion
+            FilterType.CATEGORY -> category
+            FilterType.DISTRICT -> district
+        }
+
+        val newList = currentGroup.checkedStates.toMutableList()
+        if (index in newList.indices) {
+            newList[index] = isChecked
+            onUpdate(type, newList)
+        }
+    }
+}
