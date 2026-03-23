74726ff72ff1ae96e18ed528fd296d560f966af4
s7e7a <ajunsui.peach@gmail.com>
2026-03-23 13:06:29 +0800

    new datatype to db

--- app/src/main/java/com/example/comp_3132sef/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/comp_3132sef/data/local/AppDatabase.kt
@@ -2,6 +2,13 @@ package com.example.comp_3132sef.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.comp_3132sef.data.local.Filter.SchoolsAddressDao
+import com.example.comp_3132sef.data.local.Filter.SchoolsCategoryDao
+import com.example.comp_3132sef.data.local.Filter.SchoolsDistrictDao
+import com.example.comp_3132sef.data.local.Filter.SchoolsGenderDao
+import com.example.comp_3132sef.data.local.Filter.SchoolsReligionDao
+import com.example.comp_3132sef.data.local.Filter.SchoolsSessionDao
+
 
 @Database(
     entities = [SchoolEntity::class, FavoriteEntity::class],
@@ -11,4 +18,10 @@ import androidx.room.RoomDatabase
 abstract class AppDatabase : RoomDatabase() {
     abstract fun schoolDao(): SchoolDao
     abstract fun favoriteDao(): FavoriteDao
+    abstract fun schoolsAddressDao(): SchoolsAddressDao
+    abstract fun schoolsCategoryDao(): SchoolsCategoryDao
+    abstract fun schoolsDistrictDao(): SchoolsDistrictDao
+    abstract fun schoolsGenderDao(): SchoolsGenderDao
+    abstract fun schoolsReligionDao(): SchoolsReligionDao
+    abstract fun schoolsSessionDao(): SchoolsSessionDao
 }
